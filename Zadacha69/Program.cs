f8fd8cc93ee45a15ea2288c767e401aaa815a3eb
Maxim <m.krishnevskiy@kmg.kz>
2022-07-21 19:38:11 +0600

    Seminar 9

---
+++ Zadacha69/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая на вход принимает два число А и В, и возводит число А в целую степень В с помощью рекурсии
+
+int f(int a, int b)
+{
+    if (b == 0)
+        return 1;
+    return f(a, b - 1) * a;
+}
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write(f(a, b));
---
+++ Zadacha_matrix/Program.cs
@@ -0,0 +1,90 @@
+﻿// Проверка на симметричеость матрицы 
+
+int[,] CreateArrayFirst(int m, int n)
+{
+    int[,] matrix1 = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            matrix1[i, j] = Convert.ToInt32(Console.ReadLine());
+            Console.Write(matrix1[i, j] + " \t");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+    return matrix1;
+}
+
+void CheckMatrix(int[,] matrix)
+{
+    bool flag = true;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (matrix[i, j] != matrix[j, i] && i != j) // != означает не равен
+            {
+                flag = false;
+                break;
+            }
+        }
+
+    }
+    if (flag)                                       // if (flag)  - означает сокращенная проверка
+        Console.WriteLine("yes");
+
+    else Console.WriteLine("no");
+}
+
+CheckMatrix(CreateArrayFirst(3, 3));
+
+// CheckMatrix(CreateArrayFirst(4, 4), CreateArraySecond(4, 4));
+
+// int[,] CreateMatrix(int[,] matrix)
+// {
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             //int x = Convert.ToInt32(Console.ReadLine());
+//             int x = new Random().Next(1, 100);
+//             matrix[i, j] = x;
+//         }
+//     }
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             Console.Write(matrix[i, j] + "\t");
+//         }
+//         Console.WriteLine();
+//     }
+
+//     return matrix;
+// }
+
+// void CheckMatrix(int[,] matrix)
+// {
+//     bool flag = true;
+//     Console.WriteLine();
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             if (matrix[i, j] != matrix[i, j] && i != j)
+//             {
+//                 flag = false;
+//                 break;
+//             }
+//         }
+
+//     }
+//     if (flag)
+//         Console.WriteLine("yes");
+
+//     else Console.WriteLine("no");
+
+// }
+
+
