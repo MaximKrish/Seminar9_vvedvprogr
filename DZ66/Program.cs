77c072be808bca5547d5748a1409ebc45f51fd1f
Maxim <m.krishnevskiy@kmg.kz>
2022-07-27 10:29:22 +0600

    DZ

---
+++ DZ66/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N
+
+int f(int m, int n)
+{
+    if (n == 0)
+        return 1;
+    return f(m, n - 1) + n;
+}
+Console.WriteLine("M должно быть меньше N ");
+Console.WriteLine("Введите чило M ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите чило N ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write(f(m, n));
---
+++ DZ68/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n
+
+int A(int m, int n)
+{
+    if (m == 0)
+        return n + 1;
+    if (m != 0 && n == 0)
+        return A(m - 1, 1);
+    if (m > 0 && n > 0)
+        return A(m - 1, A(n, m - 1));
+}
+
+Console.WriteLine("Введите чило M ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите чило N ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write(A(m, n));
