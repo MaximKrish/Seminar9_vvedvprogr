e4a42a48c23682b3793cdf5ca2c349610128a889
Maxim <m.krishnevskiy@kmg.kz>
2022-07-27 13:53:24 +0600

    DZ68_

--- DZ68/Program.cs
+++ DZ68/Program.cs
@@ -1,21 +1,13 @@
-﻿// Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n
-
+﻿
 int A(int m, int n)
 {
     if (m == 0)
-    {
         return n + 1;
-    }
-    else
     if (m > 0 && n == 0)
-    {
-         return A(m - 1, 1);
-    }
-    else
+        return A(m - 1, 1);
     if (m > 0 && n > 0)
-    {
         return A(m - 1, A(m, n - 1));
-    }
+    return A(m, n);
 }
 
 Console.WriteLine("Введите чило M ");
